2ef60f1bbee0d4fd6cdeea53513684e09614135e
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-21 16:18:52 +0900

    Update lv3_가장 긴 팰린드롬.py

--- Week 1/Day 5/lv3_가장 긴 팰린드롬.py
+++ Week 1/Day 5/lv3_가장 긴 팰린드롬.py
@@ -1,7 +1,23 @@
 def solution(s):
-    answer = 0
-
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
+    answer = 1
+    for left in range(len(s)):
+        for right in range(left, len(s)):
+            tmp = s[left:right+1]
+            if tmp == tmp[::-1]:
+                answer = max(answer, len(tmp))
 
     return answer
+    
+
+def solution(s):
+    if len(s) <= 1:
+        return s
+    i,l=0,0
+    for j in range(len(s)):
+        if s[j-l: j+1] == s[j-l: j+1][::-1]:
+            i, l = j-l, l+1
+            # print(s[i: i+l])
+        elif j-l > 0 and s[j-l-1: j+1] == s[j-l-1: j+1][::-1]:
+            i, l = j-l-1, l+2
+            # print(s[i: i+l])
+    return len(s[i: i+l])
